96acef9267e733ac7027db7942c2c090c94846d8
Jhony Pereira <jhony-16@live.com>
2023-11-03 12:16:53 -0300

    feat: started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./index.scss";
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
